68a83944f60b16339e3a3cde5d48c3ad96615270
Thaytubervn <garennaff42@gmail.com>
2025-06-13 19:04:50 +0700

    Add files via upload

---
+++ decoded_icons.lua
@@ -0,0 +1,124 @@
+
+local Icons = {
+  {
+    Name = "rewind",
+    AssetId = 16898613699,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 563, Y = 967}
+  },
+  {
+    Name = "fuel",
+    AssetId = 16898613353,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 196, Y = 967}
+  },
+  {
+    Name = "square-arrow-out-up-right",
+    AssetId = 16898613777,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 967, Y = 514}
+  },
+  {
+    Name = "table-cells-split",
+    AssetId = 16898613777,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 771, Y = 955}
+  },
+  {
+    Name = "gavel",
+    AssetId = 16898613353,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 967, Y = 808}
+  },
+  {
+    Name = "dna-off",
+    AssetId = 16898613044,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 453, Y = 967}
+  },
+  {
+    Name = "refresh-ccw-dot",
+    AssetId = 16898613699,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 869, Y = 404}
+  },
+  {
+    Name = "bean",
+    AssetId = 16898612629,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 967, Y = 906}
+  },
+  {
+    Name = "arrow-up-right-from-circle",
+    AssetId = 16898612629,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 563, Y = 967}
+  },
+  {
+    Name = "table-columns-split",
+    AssetId = 16898613777,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 967, Y = 808}
+  },
+  {
+    Name = "bolt",
+    AssetId = 16898612819,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 306, Y = 820}
+  },
+  {
+    Name = "square-asterisk",
+    AssetId = 16898613777,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 710, Y = 771}
+  },
+  {
+    Name = "feather",
+    AssetId = 16898613353,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 771, Y = 98}
+  },
+  {
+    Name = "align-horizontal-distribute-center",
+    AssetId = 16898612629,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 771, Y = 355}
+  },
+  {
+    Name = "align-center",
+    AssetId = 16898612629,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 0, Y = 869}
+  },
+  {
+    Name = "grip-vertical",
+    AssetId = 16898613509,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 0, Y = 869}
+  },
+  {
+    Name = "person-standing",
+    AssetId = 16898613699,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 563, Y = 771}
+  },
+  {
+    Name = "badge-swiss-franc",
+    AssetId = 16898612629,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 771, Y = 857}
+  },
+  {
+    Name = "between-horizontal-end",
+    AssetId = 16898612819,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 771, Y = 306}
+  },
+  {
+    Name = "rotate-cw",
+    AssetId = 16898613699,
+    Size = {Width = 48, Height = 48},
+    Position = {X = 869, Y = 453}
+  }
+}
+return Icons
