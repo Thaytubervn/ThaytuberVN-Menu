9efeafd478bf6cee246308e0d75211d98136aeff
Thaytubervn <garennaff42@gmail.com>
2025-06-14 20:03:50 +0700

    Update ThaytuberVN Hub.lua

--- ThaytuberVN Hub.lua
+++ ThaytuberVN Hub.lua
@@ -1644,7 +1644,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 	Main.BackgroundTransparency = 1
 	if Main:FindFirstChild('Notice') then Main.Notice.Visible = false end
 	Main.Shadow.Image.ImageTransparency = 1
-	Main.UIStroke.Transparency = 1
 	LoadingFrame.BackgroundTransparency= 1
 	LoadingFrame.Title.TextTransparency = 1
 	LoadingFrame.Subtitle.TextTransparency = 1
@@ -3560,7 +3559,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 
 	-- 🚫 Tắt loading, hiện lại nội dung chính
 	customLoadingGui.LoadingFrame.Visible = false -- để ẩn
-	Main.UIStroke.Transparency = 0
+	if Main:FindFirstChild("UIStroke") then Main.UIStroke.Enabled = true end
 	Elements.Visible = true
 
 
