f1dcb247bf4fd894990dc57f599b1a28f699950f
Thaytubervn <garennaff42@gmail.com>
2025-06-16 19:22:49 +0700

    Create Troll Animation.lua

---
+++ Troll Animation.lua
@@ -0,0 +1,19 @@
+-- Emotes with R6 and R15 variants
+return {
+    ["Bang"] = {
+        R6 = "rbxassetid://189854234",
+        R15 = "rbxassetid://10714068222",
+    },
+    ["Get Banged"] = {
+        R6 = "rbxassetid://189854234",
+        R15 = "rbxassetid://10714360343",
+    },
+    ["Suck"] = {
+        R6 = "rbxassetid://178130996",
+        R15 = "rbxassetid://5918726674",
+    },
+    ["Get Suc"] = {
+        R6 = "rbxassetid://148840371",
+        R15 = "rbxassetid://5918726674",
+    },
+}
